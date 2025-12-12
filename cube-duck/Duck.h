0b2a34ba0b5a30c3d56f0e945502267e0591bcea
luiz-couto <luizcouto.cg@gmail.com>
2025-12-12 12:44:50 +0000

    run and walk

--- cube-duck/Duck.h
+++ cube-duck/Duck.h
@@ -8,17 +8,20 @@
 #include "Window.h"
 
 #define DUCK_MODEL_FILE "models/Duck-white.gem"
-#define VELOCITY 0.01f
+#define RUN_VELOCITY 0.05f
+#define WALK_VELOCITY 0.02f
 #define LOADING_FRAME 15
 
 enum DUCK_ANIMATION {
     IDLE_VARIATION,
     WALK_FORWARD,
     TURN_90_LEFT,
-    TURN_90_RIGHT
+    TURN_90_RIGHT,
+    RUN_FORWARD,
+    WALK_BACKWARDS
 };
 
-const char *AnimationsMap[] = { "idle variation", "walk forward", "turn 90 l", "turn 90 r" };
+const char *AnimationsMap[] = { "idle variation", "walk forward", "turn 90 l", "turn 90 r", "run forward", "walk backwards" };
 
 class Duck {
 public:
@@ -48,6 +51,25 @@ public:
     }
 
     void reactToMovementKeys(Window *win) {
+        if (win->keys['W']) {
+            if (rotationAngle == 0 || rotationAngle == 360) position.z -= RUN_VELOCITY;
+            if (rotationAngle == 90) position.x -= RUN_VELOCITY;
+            if (rotationAngle == 180) position.z += RUN_VELOCITY;
+            if (rotationAngle == 270) position.x += RUN_VELOCITY;
+
+            currentAnimation = RUN_FORWARD;
+        }
+
+        if (win->keys['S']) {
+            if (rotationAngle == 0 || rotationAngle == 360) position.z += WALK_VELOCITY;
+            if (rotationAngle == 90) position.x += WALK_VELOCITY;
+            if (rotationAngle == 180) position.z -= WALK_VELOCITY;
+            if (rotationAngle == 270) position.x -= WALK_VELOCITY;
+
+            currentAnimation = WALK_BACKWARDS;
+        }
+
+
         if (loadingFrame < LOADING_FRAME) {
             loadingFrame++;
             return;
@@ -56,32 +78,32 @@ public:
             loadingFrame = 0;
         }
 
-        if (win->keys['W']) {
-            //position.z -= VELOCITY;
-        }
         if (win->keys['A']) {
             rotationAngle += 90;
-            if (rotationAngle == 360) rotationAngle = 0;
-            //currentAnimation = TURN_90_LEFT;
+            if (rotationAngle > 360) rotationAngle = 0;
+            currentAnimation = TURN_90_LEFT;
+        }
+
+        if (win->keys['D']) {
+            rotationAngle -= 90;
+            if (rotationAngle < 0) rotationAngle = 270;
+            currentAnimation = TURN_90_RIGHT;
         }
     }
 
     void updateAnimation(Window *win, float dt) {
         reactToMovementKeys(win);
         animatedInstance.update(AnimationsMap[currentAnimation], dt);
-		if (animatedInstance.animationFinished())
-		{   
-            //animatedInstance.update(AnimationsMap[IDLE_VARIATION], dt);
-			animatedInstance.resetAnimationTime();
-		}
+        if (animatedInstance.animationFinished()) {
+            currentAnimation = IDLE_VARIATION;
+            animatedInstance.resetAnimationTime();
+        }
     }
 
     void draw(Camera *camera) {
         rotation.setRotationY(rotationAngle);
         translation = translation.setTranslation(position);
         duckModel.vertexShaderCB->W = (translation.mul(rotation)).mul(scale);
-
-        //std::println("World Matrix = {}", duckModel.vertexShaderCB->W);
         duckModel.draw(core, camera, &animatedInstance);
     }
 };
